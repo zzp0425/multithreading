a682814a8785068c9444eebc4a8aab643050b91f
zhouzhengpin <zhouzhengpin>
2017-08-13 19:08:17 +0800

    countDownLatch with lambda

--- src/main/webapp/java/com/zzp/learn/concurrencyUtilClass/CountDownLatchTest.java
+++ src/main/webapp/java/com/zzp/learn/concurrencyUtilClass/CountDownLatchTest.java
@@ -1,9 +1,28 @@
 package com.zzp.learn.concurrencyUtilClass;
 
+import java.util.concurrent.CountDownLatch;
+
 /**
- * Desc
+ * Desc CountDownLatch可以实现join的功能，并且比join的功能更多
  * Created by zzp
  * on 2017/8/11.7:08
  */
 public class CountDownLatchTest {
+
+    static CountDownLatch countDownLatch = new CountDownLatch(2);
+    public static void main(String[] args) {
+        new Thread(() -> {
+                System.out.println(1);
+                countDownLatch.countDown();
+                System.out.println(2);
+                countDownLatch.countDown();
+        }).start();
+
+        try {
+            countDownLatch.await();
+            System.out.println(3);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
